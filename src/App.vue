6f21cf542b53cb07af983b91bbda3eca5e3c1e83
yusfi <yadilaksa@gmail.com>
2020-05-14 22:25:12 +0700

    add some styles

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" />
-    <router-view />
+    <div class="container">
+      <img src="./assets/logo.png" />
+      <router-view />
+    </div>
   </div>
 </template>
 
@@ -9,16 +11,8 @@
 export default {
   name: "app"
 };
-var hhalo = "halo";
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "assets/css/style.css";
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    <h1>Welcome to the Forum!</h1>
-    <div v-for="thread in threads" :key="thread">
-      <h2>{{ thread.title}}</h2>
-      <div v-for="postId in thread.posts" :key="postId">
-        <p>{{ users[posts[postId].userId].name }}</p>
-        <p>{{ posts[postId].text }}</p>
+    <div v-for="thread in threads" :key="thread.id" class="col-large push-top">
+      <h1>{{ thread.title}}</h1>
+      <div class="post-list">
+        <div v-for="postId in thread.posts" :key="postId" class="post">
+          <div class="user-info">
+            <a href="#" class="user-name">{{ users[posts[postId].userId].name }}</a>
+            <a href="#">
+              <img :src="users[posts[postId].userId].avatar" alt class="avatar-large" />
+            </a>
+            <p class="desktop-only text-small">107 posts</p>
+          </div>
+          <div class="post-content">
+            <div>{{ posts[postId].text }}</div>
+          </div>
+          <div class="post-date text-faded">{{ posts[postId].publishedAt }}</div>
+        </div>
       </div>
-      <p>===========================================</p>
     </div>
   </div>
 </template>
