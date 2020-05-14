2849a835aa27d091cdb6d9dae148174a4cd0b98b
yusfi <yadilaksa@gmail.com>
2020-05-14 21:57:17 +0700

    import data and show the threads, users, and posts

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,8 +23,9 @@ module.exports = {
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'no-unused-vars': process.env.NODE_ENV === 'production' ? 2 : 1,
-    'quotes': process.env.NODE_ENV === 'production' ? 2 : 1,
-    'semi': process.env.NODE_ENV === 'production' ? 2 : 1,
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'quotes': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'semi': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'space-before-function-paren': 0
   }
 }
