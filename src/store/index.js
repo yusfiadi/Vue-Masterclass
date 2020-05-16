cd9db2e6ccf5e71a86aaf03db73bed998433ee6a
yusfi <yadilaksa@gmail.com>
2020-05-16 20:56:42 +0700

    add getters for  authUser

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,15 @@ import sourceData from "@/data"
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: sourceData,
+  state: {
+    ...sourceData,
+    authId: 'VXjpr2WHa8Ux4Bnggym8QFLdv5C3'
+  },
+  getters: {
+    authUser(state) {
+      return state.users[state.authId]
+    }
+  },
   actions: {
     createPost(context, post) {
       const postId = `great post ${Math.random()}`
