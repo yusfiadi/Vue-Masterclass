0f52a50167b13ef757dfb297fddb3f71caf4dfc3
yusfi <yadilaksa@gmail.com>
2020-05-20 15:48:08 +0700

    fetch item from firebase for the thread page

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -1,25 +1,23 @@
 <template>
-  <div>
-    <div class="col-large push-top">
-      <h1>{{ thread.title }}</h1>
-      <router-link
-        :to="{name: 'ThreadEdit', id:'this.id'}"
-        class="btn-green btn-small"
-        tag="button"
-      >Edit Thread</router-link>
-      <p>
-        By
-        <a href="#" class="link-unstyled">{{ user.name }}</a>,
-        <app-date :timestamp="thread.publishedAt"></app-date>.
-        <span
-          style="float:right; margin-top: 2px;"
-          class="hide-mobile text-faded text-small"
-        >{{ repliesCount }} replies by {{contributorsCount}} contributors</span>
-      </p>
-      <post-list :posts="posts"></post-list>
-      <post-editor :threadId="id"></post-editor>
-      <!-- <post-editor @save="addPost" :threadId="id"></post-editor> -->
-    </div>
+  <div v-if="thread && user" class="col-large push-top">
+    <h1>{{ thread.title }}</h1>
+    <router-link
+      :to="{name: 'ThreadEdit', id:'this.id'}"
+      class="btn-green btn-small"
+      tag="button"
+    >Edit Thread</router-link>
+    <p>
+      By
+      <a href="#" class="link-unstyled">{{ user.name }}</a>,
+      <app-date :timestamp="thread.publishedAt"></app-date>.
+      <span
+        style="float:right; margin-top: 2px;"
+        class="hide-mobile text-faded text-small"
+      >{{ repliesCount }} replies by {{contributorsCount}} contributors</span>
+    </p>
+    <post-list :posts="posts"></post-list>
+    <post-editor :threadId="id"></post-editor>
+    <!-- <post-editor @save="addPost" :threadId="id"></post-editor> -->
   </div>
 </template>
 
@@ -27,6 +25,7 @@
 import firebase from "firebase";
 import PostList from "@/components/PostList";
 import PostEditor from "@/components/PostEditor";
+import { countObjectProperties } from "@/utils";
 export default {
   components: {
     PostList,
@@ -55,28 +54,59 @@ export default {
       return this.$store.getters.threadRepliesCount(this.thread[".key"]);
     },
     contributorsCount() {
-      // find the repies
-      const replies = Object.keys(this.thread.posts)
-        .filter(postId => postId !== this.thread.firstPostId)
-        .map(postId => this.$store.state.posts[postId]);
-      // get the userId
-      const userIds = replies.map(post => post.userId);
-      // count the unique ids
-      // return userIds.filter((item, index) => index === userIds.indexOf(item))
-      //   .length;
-
-      // Versi ES6
-      return [...new Set(userIds)].length;
+      return countObjectProperties(this.thread.contributors);
     }
   },
   created() {
+    // fetch thread
     firebase
       .database()
       .ref("threads")
       .child(this.id)
       .once("value", snapshot => {
         const thread = snapshot.val();
-        this.$store.commit("setThread", { threadId: snapshot.key, thread });
+        this.$store.commit("setThread", {
+          threadId: snapshot.key,
+          thread: { ...thread, ".key": snapshot.key }
+        });
+        // fetch user
+        firebase
+          .database()
+          .ref("users")
+          .child(thread.userId)
+          .once("value", snapshot => {
+            const user = snapshot.val();
+            this.$store.commit("setUser", {
+              userId: snapshot.key,
+              user: { ...user, ".key": snapshot.key }
+            });
+          });
+        Object.keys(thread.posts).forEach(postId => {
+          // fetch post
+          firebase
+            .database()
+            .ref("posts")
+            .child(postId)
+            .once("value", snapshot => {
+              const post = snapshot.val();
+              this.$store.commit("setPost", {
+                postId: snapshot.key,
+                post: { ...post, ".key": snapshot.key }
+              });
+              // fetch user
+              firebase
+                .database()
+                .ref("users")
+                .child(post.userId)
+                .once("value", snapshot => {
+                  const user = snapshot.val();
+                  this.$store.commit("setUser", {
+                    userId: snapshot.key,
+                    user: { ...user, ".key": snapshot.key }
+                  });
+                });
+            });
+        });
       });
   }
 };
