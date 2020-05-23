3ec280a2c12122e5d266ef340da4d2a1b413769c
yusfi <yadilaksa@gmail.com>
2020-05-23 13:39:46 +0700

    create register page

--- src/router/index.js
+++ src/router/index.js
@@ -1,71 +1,78 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from '@/pages/PageHome'
-import Category from '@/pages/PageCategory'
-import ThreadShow from '@/pages/PageThreadShow'
-import ThreadCreate from '@/pages/PageThreadCreate'
-import ThreadEdit from '@/pages/PageThreadEdit'
-import Forum from '@/pages/PageForum'
-import Profile from '@/pages/PageProfile'
-import NotFound from '@/pages/PageNotFound'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/pages/PageHome";
+import Category from "@/pages/PageCategory";
+import ThreadShow from "@/pages/PageThreadShow";
+import ThreadCreate from "@/pages/PageThreadCreate";
+import ThreadEdit from "@/pages/PageThreadEdit";
+import Forum from "@/pages/PageForum";
+import Register from "@/pages/PageRegister";
+import Profile from "@/pages/PageProfile";
+import NotFound from "@/pages/PageNotFound";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  routes: [{
-      path: '/',
-      name: 'Home',
+  routes: [
+    {
+      path: "/",
+      name: "Home",
       component: Home
     },
     {
-      path: '/category/:id',
-      name: 'Category',
+      path: "/category/:id",
+      name: "Category",
       component: Category,
       props: true
     },
     {
-      path: '/forum/:id',
-      name: 'Forum',
+      path: "/forum/:id",
+      name: "Forum",
       component: Forum,
       props: true
     },
     {
-      path: '/thread/create/:forumId',
-      name: 'ThreadCreate',
+      path: "/thread/create/:forumId",
+      name: "ThreadCreate",
       component: ThreadCreate,
       props: true
     },
     {
-      path: '/thread/:id',
-      name: 'ThreadShow',
+      path: "/thread/:id",
+      name: "ThreadShow",
       component: ThreadShow,
       props: true
     },
     {
-      path: '/thread/:id/edit',
-      name: 'ThreadEdit',
+      path: "/thread/:id/edit",
+      name: "ThreadEdit",
       component: ThreadEdit,
       props: true
     },
     {
-      path: '/me',
-      name: 'Profile',
+      path: "/me",
+      name: "Profile",
       component: Profile,
       props: true
     },
     {
-      path: '/me/edit',
-      name: 'ProfileEdit',
+      path: "/me/edit",
+      name: "ProfileEdit",
       component: Profile,
       props: {
         edit: true
       }
     },
     {
-      path: '*',
-      name: 'NotFound',
+      path: "/register",
+      name: "Register",
+      component: Register
+    },
+    {
+      path: "*",
+      name: "NotFound",
       component: NotFound
     }
   ],
-  mode: 'history'
-})
+  mode: "history"
+});
