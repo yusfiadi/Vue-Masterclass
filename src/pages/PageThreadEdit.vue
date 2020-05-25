f419a2ebb61755536117b6cef0babf6cbe5d4969
yusfi <yadilaksa@gmail.com>
2020-05-25 14:55:27 +0700

    add component navigation guard in  PageThreadEdit

--- src/pages/PageThreadEdit.vue
+++ src/pages/PageThreadEdit.vue
@@ -4,7 +4,7 @@
       Editing
       <i>{{thread.title}}</i>
     </h1>
-    <thread-editor :title="thread.title" :text="text" @save="save" :cancel="cancel"></thread-editor>
+    <thread-editor ref="editor" :title="thread.title" :text="text" @save="save" :cancel="cancel"></thread-editor>
   </div>
 </template>
 
@@ -31,6 +31,14 @@ export default {
     text() {
       const post = this.$store.state.posts[this.thread.firstPostId];
       return post ? post.text : null;
+    },
+    hasUnsavedChanges() {
+      // this.saved is not required in this implementation because `this.thread.title` and `this.text` are reactive
+      // Thus `hasUnsavedChanges` will automatically become false when the thread is updated
+      return (
+        this.$refs.editor.form.title !== this.thread.title ||
+        this.$refs.editor.form.text !== this.text
+      );
     }
   },
   methods: {
@@ -63,6 +71,20 @@ export default {
         this.asyncDataStatus_fetched();
       });
     });
+  },
+  beforeRouteLeave(to, from, next) {
+    if (this.hasUnsavedChanges) {
+      const confirmed = window.confirm(
+        "Are you sure you want to leave? Any unsaved changes will be lost!"
+      );
+      if (confirmed) {
+        next();
+      } else {
+        next(false);
+      }
+    } else {
+      next();
+    }
   }
 };
 </script>
